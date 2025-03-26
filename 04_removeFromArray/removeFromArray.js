4b8ccfd189a3dc0a2dc6e0f96a308209d945a849
tawana <tawanaworks@gmail.com>
2025-03-26 19:23:31 +0200

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArr = [], ...itemsToremove) {
+  // returns the input array if no values were specified to be removed
+  if (itemsToremove.length === 0) {
+    return inputArr;
+  }
+
+  let resultArr = inputArr;
+  for (let i = 0; i !== itemsToremove.length; i++) {
+    resultArr = resultArr.filter((item) => itemsToremove[i] !== item);
+    console.log(resultArr);
+  }
+  return resultArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// removeFromArray([1, 2, 3, 4, 5, 8], 8, 3);
