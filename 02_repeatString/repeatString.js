9e83005aa968081a1a2c0de3bdf7d218298ee2d8
tawana <tawanaworks@gmail.com>
2025-03-26 16:19:17 +0200

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  if (num === 0) {
+    return "";
+  }
 
+  let resultStr = "";
+  while (num !== 0) {
+    resultStr += string;
+    num--;
+  }
+  return resultStr;
 };
 
 // Do not edit below this line
