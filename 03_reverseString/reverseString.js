d2676eadbf780afa332ddbf64829631b9fd559dd
tawana <tawanaworks@gmail.com>
2025-03-26 16:19:37 +0200

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (inputStr = "") {
+  let arrStr = inputStr.split("");
+  let outStr = arrStr.reverse().join("");
+  return outStr;
 };
 
 // Do not edit below this line
