db2e3996b118e71c5fa8c67b8ad3133db7587135
tawana <tawanaworks@gmail.com>
2025-04-05 15:33:49 +0200

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9;
+const CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5;
+const FAHRENHEIT_OFFSET = 32;
+
+const convertToCelsius = (fahrenheitTemperature) => {
+  let celsius = ((fahrenheitTemperature - FAHRENHEIT_OFFSET) * (FAHRENHEIT_TO_CELSIUS_FACTOR)).toFixed(1);
+  return parseFloat(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (celsiusTemperature) => {
+  let fahrenheit = ((celsiusTemperature * (CELSIUS_TO_FAHRENHEIT_FACTOR)) + FAHRENHEIT_OFFSET).toFixed(1);
+  return parseFloat(fahrenheit);
 };
 
 // Do not edit below this line
